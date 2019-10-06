e8eadf0ad428b7201f1f1ab54d2d5e298f9f803b
unknown <armandodejesus678@gmail.com>
2019-10-06 16:19:38 -0500

    botones de cobrar funcionalidad agregado

--- client/components/client/index.jsx
+++ client/components/client/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import style from './style';
 import usePaid from '../../hocks/use_paid';
+import { withRouter } from 'react-router-dom';
 
 function Client (props){
 
   const is_paid = usePaid(props.latest_pay);
 
+  const handleClick = () => {
+    if (is_paid) return false;
+    props.history.push(`/cobrar/${props.id}`);
+  }
 
   return (
     <article className={style.item}>
@@ -21,10 +26,9 @@ function Client (props){
       <div className={style.const}>
         $ {props.cost}.00
       </div>
-      <button className={is_paid ? style.button_paid :  style.button}>{is_paid ? 'Pagado': 'Cobrar'}</button>
-
+      <button onClick={handleClick} className={is_paid ? style.button_paid :  style.button}>{is_paid ? 'Pagado': 'Cobrar'}</button>
     </article>
   )
 }
 
-export default Client;
+export default withRouter(Client);
