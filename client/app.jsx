99f39200ddcc51f0bb5269a7000cabf5ad90eb24
unknown <armandodejesus678@gmail.com>
2019-10-06 14:33:32 -0500

    lista de pagados agregado

--- client/app.jsx
+++ client/app.jsx
@@ -6,6 +6,7 @@ import {Route, Switch} from 'react-router-dom';
 
 import Clients from './pages/clients';
 import Login from './pages/login';
+import Paids from './pages/paids';
 
 
 function App (props) {
@@ -16,6 +17,7 @@ function App (props) {
         <Route path='/' exact={true} component={Clients} />
         <Route path='/clientes' exact={true} component={Clients} />
         <Route path='/login' exact={true} component={Login} />
+        <Route path='/pagados' exact={true} component={Paids} />
       </Switch>
     </div>
   )
