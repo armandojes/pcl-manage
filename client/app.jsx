9b63c2bc1d4eda1e5c1d274c7201894b8f723688
unknown <armandodejesus678@gmail.com>
2019-10-06 14:37:45 -0500

    lista de no pagados integrado

--- client/app.jsx
+++ client/app.jsx
@@ -7,6 +7,7 @@ import {Route, Switch} from 'react-router-dom';
 import Clients from './pages/clients';
 import Login from './pages/login';
 import Paids from './pages/paids';
+import NotPaids from './pages/not_paids';
 
 
 function App (props) {
@@ -18,6 +19,7 @@ function App (props) {
         <Route path='/clientes' exact={true} component={Clients} />
         <Route path='/login' exact={true} component={Login} />
         <Route path='/pagados' exact={true} component={Paids} />
+        <Route path='/nopagados' exact={true} component={NotPaids} />
       </Switch>
     </div>
   )
--- client/pages/not_paids/index.jsx
+++ client/pages/not_paids/index.jsx
@@ -5,19 +5,19 @@ import List from '../../components/list';
 import Loading from '../../components/loading_page';
 import moment from '../../helpers/moment';
 
-function Paids (props){
+function NotPaids (props){
 
   useEffect(() => {
   !props.items.length && props.fetch_clients();
   },[])
-  
+
   if (props.loading) return (<Loading />)
   return (<List {...props}/>)
 }
 
 const mapStateToProps = (state) => {
-  const clients_paid = state.clients.items.filter(client => client.latest_pay === moment.get_period());
+  const clients_paid = state.clients.items.filter(client => client.latest_pay !== moment.get_period());
   return {...state.clients, items: clients_paid};
 }
 
-export default connect(mapStateToProps, {fetch_clients})(Paids);
+export default connect(mapStateToProps, {fetch_clients})(NotPaids);
