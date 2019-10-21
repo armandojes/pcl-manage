86cf6b7c32e0fe189b842f82b7ee17d895c1ba5a
unknown <armandodejesus678@gmail.com>
2019-10-21 17:39:00 -0500

    soporte para cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './network/routes';
 import response from './middlewares/response';
+import cors from 'cors';
 
 //server statics
 const server = express();
 server.use('/public', express.static('public'));
 server.use(express.json());
+server.use(cors());
 server.use(response);
 
 routes(server);
