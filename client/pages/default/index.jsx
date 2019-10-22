015741ddff85822c721b5d6f4ac6f14e28fc12cd
unknown <armandodejesus678@gmail.com>
2019-10-22 17:12:34 -0500

    pantalla principal

--- client/pages/default/index.jsx
+++ client/pages/default/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Container from '../../components/container';
+import { connect } from 'react-redux';
 import style from './style';
 
 
 function Default (props){
   return (
-    <Container>
-      hello default
+
+    <Container className={style.container}>
+      <img src={`${ASSETS}/logo.png`}/>
+      <h1 className={style.title}>Sucursal {props.name}</h1>
     </Container>
+
   )
 }
 
-export default Default;
+const  mapStateToProps = (state) => {
+  return state.session
+}
+
+export default connect(mapStateToProps)(Default);
