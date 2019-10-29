ebc739bcc28715e17a12796d253513cb62d56bf7
unknown <armandodejesus678@gmail.com>
2019-10-29 11:03:11 -0600

    integracion de config .js

---
+++ server/config.example.js
@@ -0,0 +1,20 @@
+//production database
+// rename this file to config.js
+export const database = {
+  host: 'test',
+  user: 'test',
+  database: 'test',
+  password:'test',
+  port: 3306,
+}
+
+
+export const jsonwebtoken = {
+  key: 'test',
+  session_time: 'test',
+}
+
+export default {
+  database,
+  jsonwebtoken,
+};
